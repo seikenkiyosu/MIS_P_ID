318795a7c7b4875178bf850fa746f3b3c429e5e7
Seiken <seikenkiyosu@icloud.com>
2015-11-04 12:16:00 +0900

    changed interaction

--- src/MIS_P_IS_simulator/Interaction.java
+++ src/MIS_P_IS_simulator/Interaction.java
@@ -1,5 +1,7 @@
 package MIS_P_IS_simulator;
 
+import javax.rmi.CORBA.Tie;
+
 import Agent.Agent;
 import Agent.InfoIndependentNode;
 
@@ -7,79 +9,67 @@ public class Interaction {
 	public static void interaction(Agent x, Agent y){
 		//lid更新プロセス
 		//どっちかがIIn空のとき
-		if (x.IIn == null && y.IIn != null) { 
-				x.IIn = new InfoIndependentNode();
-				x.IIn.var = y.IIn.var;
-				x.IIn.hop = y.IIn.hop + 1;
-				x.IIn.timer_IIn = y.IIn.timer_IIn;
-			}
-		if (x.IIn != null && y.IIn == null) { 
-				y.IIn = new InfoIndependentNode();
-				y.IIn.var = x.IIn.var;
-				y.IIn.hop = x.IIn.hop + 1;
-				y.IIn.timer_IIn = x.IIn.timer_IIn;
-			}
+		if (x.IIn == null || (x.IIn.var == y.IIn.var && x.IIn.hop > y.IIn.hop+1)) {	//xのIInが書き換えられる 
+			x.IIn = new InfoIndependentNode();
+			x.IIn.var = y.IIn.var;
+			x.IIn.hop = y.IIn.hop + 1;
+			x.IIn.timer_IIn = y.IIn.timer_IIn;
+		}
+		if (y.IIn == null || (y.IIn.var == x.IIn.var && y.IIn.hop > x.IIn.hop+1)) {	//yのIInが書き換えられる 
+			y.IIn = new InfoIndependentNode();
+			y.IIn.var = x.IIn.var;
+			y.IIn.hop = x.IIn.hop + 1;
+			y.IIn.timer_IIn = x.IIn.timer_IIn;
+		}
 		//両方IInが空でないとき
-		if (x.IIn != null || y.IIn !=null) {	//両方nullなら意味ない
-			if (x.IIn.var == y.IIn.var) {	//IInの独立ノードのidが同じとき
-				if (x.IIn.hop > y.IIn.hop+1) {
+		if (x.IIn != null && y.IIn !=null) {	//両方nullなら意味ない
+			if (x.IIn.hop + y.IIn.hop + 1 <= MIS_P_IS_simulator.k) {	//hop数の和がk以下のとき
+				if (x.IIn.var > y.IIn.var) {
 					x.IIn.var = y.IIn.var;
 					x.IIn.hop = y.IIn.hop + 1;
 					x.IIn.timer_IIn = y.IIn.timer_IIn;
 				}
-				else if ( y.IIn.hop > x.IIn.hop+1) {
+				else if (x.IIn.var < y.IIn.var) {
 					y.IIn.var = x.IIn.var;
 					y.IIn.hop = x.IIn.hop + 1;
 					y.IIn.timer_IIn = x.IIn.timer_IIn;
 				}
 			}
-			else {		//IInの独立ノードのidが異なるとき
-				if (x.IIn.hop + y.IIn.hop + 1 <= MIS_P_IS_simulator.k) {
-					if (x.IIn.var > y.IIn.var) {
-						x.IIn.var = y.IIn.var;
-						x.IIn.hop = y.IIn.hop + 1;
-						x.IIn.timer_IIn = y.IIn.timer_IIn;
-					}
-					else if (x.IIn.var < y.IIn.var) {
-						y.IIn.var = x.IIn.var;
-						y.IIn.hop = x.IIn.hop + 1;
-						y.IIn.timer_IIn = x.IIn.timer_IIn;
-					}
+			else {		//hop数の和がkより大きい
+				if (x.IIn.hop > y.IIn.hop+1) {
+					x.IIn.var = y.IIn.var;
+					x.IIn.hop = y.IIn.hop + 1;
+					x.IIn.timer_IIn = y.IIn.timer_IIn;
 				}
-				else {
-					if (x.IIn.hop > y.IIn.hop+1) {
-						x.IIn.var = y.IIn.var;
-						x.IIn.hop = y.IIn.hop + 1;
-						x.IIn.timer_IIn = y.IIn.timer_IIn;
-					}
-					else if (y.IIn.hop > x.IIn.hop+1) {
-						y.IIn.var = x.IIn.var;
-						y.IIn.hop = x.IIn.hop + 1;
-						y.IIn.timer_IIn = x.IIn.timer_IIn;
-					}
+				else if (y.IIn.hop > x.IIn.hop+1) {
+					y.IIn.var = x.IIn.var;
+					y.IIn.hop = x.IIn.hop + 1;
+					y.IIn.timer_IIn = x.IIn.timer_IIn;
 				}
 			}
+		}
+		
 		//falseなInと嘘の独立ノード(削除された独立ノードの情報)を消す 
 //			if (x.IIn.var == y.IIn.var) {
 				x.IIn.timer_IIn = y.IIn.timer_IIn = max(x.IIn.timer_IIn-1, y.IIn.timer_IIn-1, 0);
 //			}
 			//IInの独立ノードと交流したらtimer_IInをリセット
-			if (x.IIn.var == y.IIn.var && y.IsIndependentNode()) { x.IIn.timer_IIn = MIS_P_IS_simulator.t_max; }
-			if (y.IIn.var == x.IIn.var && x.IsIndependentNode()) { y.IIn.timer_IIn = MIS_P_IS_simulator.t_max; }
+			if (x.IIn.var == y.IIn.var && (x.IsIndependentNode()||y.IsIndependentNode()) ) { x.IIn.timer_IIn = y.IIn.timer_IIn =  MIS_P_IS_simulator.t_max; }
+			else if (x.IIn.var != y.IIn.var) { x.IIn.timer_IIn--; y.IIn.timer_IIn--; }	//IInのidが違うときは不安度が増していく
 			//タイムアウトしたらIInの情報を忘れる
 			if (x.IIn.timer_IIn == 0) { x.IIn = null; }
 			if (y.IIn.timer_IIn == 0) { y.IIn = null; }
-		}
 		
-		//Inを減らすためのプロセス(k-hop以内にInが存在し，idがそれより低ければ，降りる)
-		if (x.IIn != null && y.IIn !=null) {
-			if (x.IsIndependentNode() && x.var != y.IIn.var && y.IIn.hop < MIS_P_IS_simulator.k) {
-				if (x.var > y.IIn.var) x.IIn = null;
-			}
-			if (y.IsIndependentNode() && y.var != x.IIn.var && x.IIn.hop < MIS_P_IS_simulator.k) {
-				if (y.var > x.IIn.var) y.IIn = null;
-			}
-		}
+		
+//		//Inを減らすためのプロセス(k-hop以内にInが存在し，idがそれより低ければ，降りる)
+//		if (x.IIn != null && y.IIn !=null) {
+//			if (x.IsIndependentNode() && x.var != y.IIn.var && y.IIn.hop < MIS_P_IS_simulator.k) {
+//				if (x.var > y.IIn.var) x.IIn = null;
+//			}
+//			if (y.IsIndependentNode() && y.var != x.IIn.var && x.IIn.hop < MIS_P_IS_simulator.k) {
+//				if (y.var > x.IIn.var) y.IIn = null;
+//			}
+//		}
 		
 		//Inを増やすためのプロセス
 		//独立ノードが見当たらなければtimer_ISをデクリメント
